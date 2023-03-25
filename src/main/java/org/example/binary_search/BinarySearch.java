a3afeccf4e9420972aae92643239d5a72d2b4b81
asharMehmood2211004KHICND <ashar7mehmood@gmail.com>
2023-03-25 22:09:29 +0500

    added binary search

---
+++ src/main/java/org/example/binary_search/BinarySearch.java
@@ -0,0 +1,29 @@
+package org.example.binary_search;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-99,-44,-6,0,2,3,6,21,52,98};
+        int ans = binarySearch(arr,2);
+        System.out.println(ans);
+    }
+    static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end){
+            //find the middle element
+//            int mid = (start + end)/2;  // might exceed the int limit in java
+            int mid = start + (end-start)/2;
+
+            if (target < arr[mid]){
+                end = mid - 1;
+            } else if (target > arr[mid]){
+                start = mid + 1;
+            }else {
+                return mid;
+            }
+
+
+        }
+        return -1;
+    }
+}
