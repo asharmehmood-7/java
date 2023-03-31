0465ea51843f2ba5415e15b302d486fbb0373c68
asharMehmood2211004KHICND <ashar7mehmood@gmail.com>
2023-03-31 23:20:08 +0500

    added binary search with recursion

---
+++ src/main/java/org/example/recursion/RecursionBinarySearch.java
@@ -0,0 +1,25 @@
+package org.example.recursion;
+
+public class RecursionBinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,5,23,88};
+        int target = 2;
+        int a = search(arr,target,0,arr.length-1);
+        System.out.println(a);
+
+    }
+    static int search(int[] arr, int target, int s, int e){
+        if (s>e){
+            return -1;
+        }
+        int m = s + (e-s)/2;
+        if (arr[m]==target){
+            return m;
+        }
+        if (arr[m]>target){
+            return search(arr,target,s,m-1);
+        }
+        return search(arr,target,m+1,e);
+    }
+}
