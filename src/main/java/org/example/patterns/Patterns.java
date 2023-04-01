84e7513ee17d2bc75d1e0b4c1f5b0d2df4bb8e8e
asharMehmood2211004KHICND <ashar7mehmood@gmail.com>
2023-04-02 00:00:41 +0500

    added triangle pattern

--- src/main/java/org/example/patterns/Patterns.java
+++ src/main/java/org/example/patterns/Patterns.java
@@ -2,15 +2,36 @@ package org.example.patterns;
 
 public class Patterns {
     public static void main(String[] args) {
-        pattern3(5);
+        pattern5(5);
+
+    }
+
+    static void pattern5(int n) {
+        int count = 1;
+        for (int row = 1; row <= (2 * n) - 1; row++) {
+
+            if (row <= n) {
+                for (int col = 1; col <= row; col++) {
+                    System.out.print("* ");
+                }
+            } else {
+                for (int col2 = 1; col2 <= (n - count); col2++) {
+                    System.out.print("* ");
+
+                }
+                count++;
+            }
+
+            System.out.println();
+        }
 
     }
 
 
-    static void pattern1(int n){
-        for (int row = 1; row <= n; row++ ){
+    static void pattern1(int n) {
+        for (int row = 1; row <= n; row++) {
 
-            for (int col = 1; col<=n; col++){  // or a col <= a constant num
+            for (int col = 1; col <= n; col++) {  // or a col <= a constant num
                 System.out.print("* ");
 
             }
@@ -18,9 +39,9 @@ public class Patterns {
         }
     }
 
-    static void pattern2(int n){
-        for (int row = 1; row <= n ; row++ ){
-            for (int col = 1; col<= row; col++  ){
+    static void pattern2(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
                 System.out.print("* ");
             }
             //after every row we want to break the line
@@ -28,11 +49,21 @@ public class Patterns {
         }
     }
 
-    static void pattern3(int n){
-        for (int row = 1; row <= n; row++){
+    static void pattern3(int n) {
+        for (int row = 1; row <= n; row++) {
 
-            for (int col = 0; col<=n-row;col++){
+            for (int col = 0; col <= n - row; col++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n) {
+        for (int row = 1; row <= n; row++) {
+
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col);
             }
             System.out.println();
         }
