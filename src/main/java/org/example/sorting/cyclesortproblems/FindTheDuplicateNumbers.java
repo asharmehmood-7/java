58b1efd253a42cdbb9c4b6d0822a6f511fbb6bff
asharMehmood2211004KHICND <ashar7mehmood@gmail.com>
2023-04-12 13:18:33 -0400

    added find duplicates with constant extra space

--- src/main/java/org/example/sorting/cyclesortproblems/FindTheDuplicateNumbers.java
+++ src/main/java/org/example/sorting/cyclesortproblems/FindTheDuplicateNumbers.java
@@ -1,17 +1,60 @@
 package org.example.sorting.cyclesortproblems;
 
+
+//Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+//
+//        There is only one repeated number in nums, return this repeated number.
+//
+//        You must solve the problem without modifying the array nums and uses only constant extra space.
+//
+
+//Input: nums = [1,3,4,2,2]
+//        Output: 2
+//
+//take XOR of all the numbers.You will get the sum with out the duplicated number.
+//
+//        (sum of all n - above sum) will give you the number   --> check this approach too
+
+
+import java.util.Arrays;
+
 public class FindTheDuplicateNumbers {
     public static void main(String[] args) {
 
+        int[] nums = {3,1,3,4,2};
+      int result =   findDuplicate(nums);
+        System.out.println(Arrays.toString(nums));
+        System.out.println(result);
     }
+
     static int findDuplicate(int[] nums) {
         int i = 0;
-        while(i<nums.length){
+        while (i < nums.length) {
             int correctIndex = nums[i] - 1;
-            if (nums[i]!=nums[correctIndex]){
+            if (nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else {
+                i++;
             }
         }
-        return 1;
+
+        for (int j = 0; j < nums.length; j++) {
+//            int correctIndex = nums[j] - 1;
+            if (nums[j] != j + 1) {
+                return nums[j];
+            }
+        }
+
+        return -1;
 
 
-}}
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+
+}
